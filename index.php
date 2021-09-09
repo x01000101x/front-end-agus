7445c2082acbaddf3128db8d77e4082b196cb505
x01000101x <leodamanik619@gmail.com>
2021-09-09 10:33:39 +0700

    initial

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Fitness App</title>
+</head>
+
+<body>
+
+    <div class="container mt-3">
+        <h1>MY FITNESS APP</h1>
+    </div>
+
+</body>
+
+</html>
